742557ef136590afc58e11e68e554ebdffdf7480
Justin_Jung <jungdw0624@gmail.com>
2019-04-16 18:16:07 +0900

    change color, lineWidth

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
 const context = canvas.getContext("2d");
+const color = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
 
 context.strokeStyle = "black";
 context.lineWidth = 2.5;
@@ -28,9 +30,22 @@ function onMouseMove(e){
     }
 }
 
-if(canvas){
+function handleColorClick(e){
+    const palette = e.target.style.backgroundColor;
+    context.strokeStyle = palette;
+}
+
+function pencil(e){
+    context.lineWidth = range.value;
+}
+
+if(canvas){ 
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPaining);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(color).forEach(temp => temp.addEventListener('click', handleColorClick));
+
+range.addEventListener('click',pencil);
